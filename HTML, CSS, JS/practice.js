f2b97ebdfc96f446b8a51d0ca1c4c4fe9c5cd940
Mad At <bhrantiknagar@gmail.com>
2026-01-05 15:22:45 +0530

    1,

--- HTML, CSS, JS/practice.js
+++ HTML, CSS, JS/practice.js
@@ -1,2 +1,19 @@
 console.log("Hello, World!"); // this is used in print 
 console.log("Here, We start!");
+console.log(" here we start again bro");
+
+// veriables 
+halfName = "Mad";
+console.log(halfName);
+age = 19;
+console.log(age);
+price = 12345.78;
+console.log(price);
+x = null;
+y = undefined;
+console.log(x);
+console.log(y);
+
+isFollow = true;      /*  boolean variables */
+isFollow = false;
+console.log(isFollow);
