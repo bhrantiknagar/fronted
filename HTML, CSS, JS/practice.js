b3b95308da0021960a40401b510cc67eb245b76b
Mad At <bhrantiknagar@gmail.com>
2026-01-05 16:04:17 +0530

    4,

--- HTML, CSS, JS/practice.js
+++ HTML, CSS, JS/practice.js
@@ -2,11 +2,13 @@ console.log("Hello, World!"); // this is used in print
 console.log("Here, We start!");
 console.log(" here we start again bro");
 
+
 // veriables 
-halfName = "Mad";
-console.log(halfName);
-age = 19;
-console.log(age);
+
+// halfName = "Mad";
+// console.log(halfName);
+// age = 19;
+// console.log(age);
 price = 12345.78;
 console.log(price);
 x = null;
@@ -14,6 +16,21 @@ y = undefined;
 console.log(x);
 console.log(y);
 
-isFollow = true;      /*  boolean variables */
+isFollow = true;           // boolean variables 
 isFollow = false;
 console.log(isFollow);
+
+// assignment operator '='
+
+let halfName = "pen";      // this is declare only once 
+halfName = "half";
+halfName = "Full";
+console.log(halfName);
+
+const age = "18";          // this is cant re-declare 
+// age = "19";
+console.log(age);
+
+
+// Data types 
+
